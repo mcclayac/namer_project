e3c953414abe3e7ff517b1762fd23a8163c5cf8d
Anthony McClay <anthony.mcclay@gmail.com>
2023-11-17 01:50:12 -0600

    Added Favorites Page

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default :
         throw UnimplementedError('No Widget for $selectedIndex');
@@ -120,6 +120,34 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text ('You have '
+               '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+
+}
 
 class GeneratorPage extends StatelessWidget {
   @override
